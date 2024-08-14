2567bc45a9b78137b9b00dec36f7934bf517efb1
Benjamin Bengfort <benjamin@rotational.io>
2024-08-14 14:24:46 -0500

    Data Records and Lists (#6)

--- envoy/resource.py
+++ envoy/resource.py
@@ -6,6 +6,7 @@ delete. Most interactions with the Envoy API are via a resource object.
 
 from envoy import client
 from envoy.exceptions import ValidationError
+from envoy.records import Record, PaginatedRecords
 
 
 class Resource(object):
@@ -13,6 +14,9 @@ class Resource(object):
     Resource objects are not intended to be used directly but are intended to be
     subclassed by resources to provide the standard functionality for REST operations.
     """
+
+    RecordType = Record
+    RecordListType = PaginatedRecords
 
     def __init__(self, client : "client.Client"):
         self.client = client
@@ -22,29 +26,42 @@ class Resource(object):
         raise AttributeError("subclasses should define the endpoint for their resource")
 
     def list(self, params: dict = None) -> list[dict]:
-        return self.client.get(
-            self.endpoint, params=params, require_authentication=True
+        reply = self.client.get(
+            *self._endpoint(), params=params, require_authentication=True
         )
 
+        collection_key = [key for key in reply.keys() if key != "page"][0]
+        return self.RecordListType(page=reply["page"], collection=reply[collection_key])
+
     def create(self, data: dict) -> dict:
-        return self.client.post(
-            data, self.endpoint, require_authentication=True
+        return self.RecordType(
+            self.client.post(data, *self._endpoint(), require_authentication=True)
         )
 
     def detail(self, rid: str, params: dict = None) -> dict:
-        return self.client.get(
-            self.endpoint, rid, params=params, require_authentication=True
+        return self.RecordType(
+            self.client.get(
+                *self._endpoint(), rid, params=params, require_authentication=True
+            )
         )
 
     def update(self, data: dict) -> dict:
         if "id" not in data:
             raise ValidationError("an ID is required to update this resource")
 
-        return self.client.put(
-            data, self.endpoint, data["id"], require_authentication=True
+        return self.RecordType(
+            self.client.put(
+                data, *self._endpoint(), data["id"], require_authentication=True
+            )
         )
 
     def delete(self, rid: str, params: dict = None) -> dict | None:
         return self.client.delete(
-            self.endpoint, rid, params=params, require_authentication=True
+            *self._endpoint(), rid, params=params, require_authentication=True
         )
+
+    def _endpoint(self):
+        endpoint = self.endpoint
+        if isinstance(endpoint, str):
+            return (endpoint,)
+        return endpoint
---
+++ tests/test_records.py
@@ -0,0 +1,84 @@
+"""
+Tests for the envoy.records module
+"""
+
+from envoy.records import *
+
+
+def test_transaction_record(transaction):
+    record = Record(transaction)
+    assert record.data == transaction
+    assert len(record) == 15
+
+    fields = record.fields()
+    assert len(fields) == 15
+
+    for field in fields:
+        item = record[field]
+        if field == "envelope_count":
+            assert isinstance(item, int)
+
+        elif field == "amount":
+            assert isinstance(item, float)
+
+        else:
+            assert isinstance(item, str)
+
+    data = record.asdict()
+    assert data is not transaction
+
+
+def test_transaction_record_list(transactions):
+    records = RecordList(transactions)
+    assert records.data == transactions
+    assert len(records) == 4
+
+    for record in records:
+        assert isinstance(record, Record)
+
+    data = records.copy()
+    assert data is not transactions
+
+
+def test_record_list_of_record_lists():
+    records = RecordList(
+        [
+            [
+                {"name": "Edgar Fromage", "age": 49, "profession": "Tailor"},
+                {"name": "Mary Tillman", "age": 21, "profession": "Tinker"},
+            ],
+            [
+                {"name": "Rachel Benedict", "age": 22, "profession": "Soldier"},
+                {"name": "Morgan Stillton", "age": 61, "profession": "Spy"},
+            ],
+        ]
+    )
+
+    assert len(records) == 2
+    for item in records:
+        assert len(item) == 2
+        assert isinstance(item, RecordList)
+
+        for sub in item:
+            assert len(item) == 2
+            assert isinstance(sub, Record)
+
+
+def test_record_of_record_lists():
+    record = Record(
+        {
+            "nested": {
+                "people": [
+                    {"name": "Edgar Fromage", "age": 49, "profession": "Tailor"},
+                    {"name": "Mary Tillman", "age": 21, "profession": "Tinker"},
+                ],
+                "fruits": [
+                    "apple", "pear", "orange", "grapes"
+                ]
+            },
+        }
+    )
+
+    assert isinstance(record["nested"], Record)
+    assert isinstance(record["nested"]["people"], RecordList)
+    assert isinstance(record["nested"]["fruits"], list)
